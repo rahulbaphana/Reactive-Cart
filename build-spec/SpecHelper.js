2b4ecab949725e22294c396331b6dd0d2bcecbe6
Rahul Baphana <rahul.kbaphana@yahoo.com>
2015-05-26 15:48:12 +0530

    Ignore .idea

--- build-spec/SpecHelper.js
+++
@@ -1,5 +0,0 @@
-React.addons.TestUtils.findRenderedDOMComponentById = function(root, id){
-    return React.addons.TestUtils.findAllInRenderedTree(root, function(ele){
-        return ele.getDOMNode().getAttribute('id') === id;
-    })[0];
-};
