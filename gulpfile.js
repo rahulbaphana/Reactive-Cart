4e4db482cc1a54397d48ded56153fc457475e9d3
Rahul Baphana <rahul.kbaphana@yahoo.com>
2015-05-26 16:24:35 +0530

    Added source maps to build task

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,25 @@ var gulp = require('gulp');
 var react = require('gulp-react');
 var watch = require('gulp-watch');
 var babel = require("gulp-babel");
-
+var sourcemaps = require("gulp-sourcemaps");
 
 gulp.task('compile-source', function () {
     return gulp.src('src/*.jsx')
         .pipe(watch('src/*.jsx'))
+        .pipe(sourcemaps.init())
         .pipe(react())
         .pipe(babel())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('js/build'));
 });
 
 gulp.task('compile-tests-spec', function () {
     return gulp.src('spec/*.jsx')
         .pipe(watch('spec/*.jsx'))
+        .pipe(sourcemaps.init())
         .pipe(react())
         .pipe(babel())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('build-spec'));
 });
 
