f27b0be9fad9224f4c3aaecd4dc6dc2d00c51a15
Rahul Baphana <rahul.kbaphana@yahoo.com>
2015-05-26 15:48:59 +0530

    Compile ES6 to 5 using Babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,23 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
-var watch = require('gulp-watch')
+var watch = require('gulp-watch');
+var babel = require("gulp-babel");
 
 
-gulp.task('compile-source', function(){
+gulp.task('compile-source', function () {
     return gulp.src('src/*.jsx')
-    .pipe(watch('src/*.jsx'))
-    .pipe(react())
-    .pipe(gulp.dest('js/build'));
+        .pipe(watch('src/*.jsx'))
+        .pipe(react())
+        .pipe(babel())
+        .pipe(gulp.dest('js/build'));
 });
 
-gulp.task('compile-tests-spec', function(){
+gulp.task('compile-tests-spec', function () {
     return gulp.src('spec/*.jsx')
-    .pipe(watch('spec/*.jsx'))
-    .pipe(react())
-    .pipe(gulp.dest('build-spec'));
+        .pipe(watch('spec/*.jsx'))
+        .pipe(react())
+        .pipe(babel())
+        .pipe(gulp.dest('build-spec'));
 });
 
-gulp.task('default', ['compile-source','compile-tests-spec']);
+gulp.task('default', ['compile-source', 'compile-tests-spec']);
