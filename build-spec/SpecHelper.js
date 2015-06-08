5529c9ab5bb5781a87aa91f726ad367cf15da0d1
Rahul Baphana <rahul.kbaphana@yahoo.com>
2015-06-08 12:06:18 +0530

    Remove build files

--- build-spec/SpecHelper.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-React.addons.TestUtils.findRenderedDOMComponentById = function (root, id) {
-    return React.addons.TestUtils.findAllInRenderedTree(root, function (ele) {
-        return ele.getDOMNode().getAttribute('id') === id;
-    })[0];
-};
